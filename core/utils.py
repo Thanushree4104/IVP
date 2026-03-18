2246dff2139622f9f3071a1f154aef8b88026e14
Thanushree4104 <150358676+Thanushree4104@users.noreply.github.com>
2026-03-18 20:06:07 +0530

    Create utils.py

---
+++ core/utils.py
@@ -0,0 +1,9 @@
+def text_to_bits(text):
+    return ''.join(format(ord(c), '08b') for c in text)
+
+def bits_to_text(bits):
+    chars = []
+    for i in range(0, len(bits), 8):
+        byte = bits[i:i+8]
+        chars.append(chr(int(byte, 2)))
+    return ''.join(chars)
