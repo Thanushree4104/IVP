f82276e6272a80846fd952999101b713017f0fb4
Thanushree4104 <150358676+Thanushree4104@users.noreply.github.com>
2026-03-18 20:06:51 +0530

    Create dct_embed.py

---
+++ core/dct_embed.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+from config import BLOCK_SIZE, STRENGTH
+from core.utils import text_to_bits
+
+def embed_watermark(image, watermark_text):
+    bits = text_to_bits(watermark_text)
+    h, w = image.shape
+
+    bit_index = 0
+
+    for i in range(0, h, BLOCK_SIZE):
+        for j in range(0, w, BLOCK_SIZE):
+
+            if bit_index >= len(bits):
+                break
+
+            block = image[i:i+BLOCK_SIZE, j:j+BLOCK_SIZE]
+            dct_block = cv2.dct(block)
+
+            if bits[bit_index] == '1':
+                dct_block[4][4] += STRENGTH
+            else:
+                dct_block[4][4] -= STRENGTH
+
+            image[i:i+BLOCK_SIZE, j:j+BLOCK_SIZE] = cv2.idct(dct_block)
+            bit_index += 1
+
+    return np.uint8(image)
